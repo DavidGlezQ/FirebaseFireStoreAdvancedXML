ee00aee0e646f0a631ab5612e94267ead95dd3ce
2350348 <DGonzalezQuezada@carnival.com>
2024-11-05 22:48:57 -0600

    add product login viewmodel

--- app/src/main/java/com/david/glez/firestoreadvancedxml/ui/addproduct/AddProductActivity.kt
+++ app/src/main/java/com/david/glez/firestoreadvancedxml/ui/addproduct/AddProductActivity.kt
@@ -2,19 +2,24 @@ package com.david.glez.firestoreadvancedxml.ui.addproduct
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.FileProvider
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.david.glez.firestoreadvancedxml.R
 import com.david.glez.firestoreadvancedxml.databinding.ActivityAddProductBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 @AndroidEntryPoint
 class AddProductActivity : AppCompatActivity() {
@@ -25,6 +30,14 @@ class AddProductActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddProductBinding
     private val addProductViewModel: AddProductViewModel by viewModels()
+
+    private lateinit var uri: Uri
+
+    private val intentCameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        if(it && uri.path?.isNotEmpty() == true) {
+            addProductViewModel.onImageSelected(uri)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +52,47 @@ class AddProductActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
+        binding.etName.doOnTextChanged { text, _, _, _ ->
+            addProductViewModel.onNameChanged(text)
+        }
 
+        binding.etDescription.doOnTextChanged { text, _, _, _ ->
+            addProductViewModel.onDescriptionChanged(text)
+        }
+
+        binding.etPrice.doOnTextChanged { text, _, _, _ ->
+            addProductViewModel.onPriceChanged(text)
+        }
+
+        binding.etImage.setOnClickListener {
+            takePicture()
+        }
+    }
+
+    private fun takePicture() {
+        generateUri()
+        intentCameraLauncher.launch(uri)
+    }
+
+    private fun generateUri() {
+        uri = FileProvider.getUriForFile(
+            Objects.requireNonNull(this),
+            "com.david.glez.firestoreadvancedxml",
+            createFile()
+        )
+    }
+
+    private fun createFile(): File {
+        val name: String = SimpleDateFormat("yyyyMMdd_hhmmss").format(Date()) + "image"
+        return File.createTempFile(name, ".jpg", externalCacheDir)
     }
 
     private fun initUIState() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 addProductViewModel.uiState.collect { state ->
-
+                    binding.pbLoading.isVisible = state.isLoading
+                    binding.btnAddProduct.isEnabled = state.isValidProduct()
                 }
             }
         }
--- app/src/main/java/com/david/glez/firestoreadvancedxml/ui/addproduct/AddProductViewModel.kt
+++ app/src/main/java/com/david/glez/firestoreadvancedxml/ui/addproduct/AddProductViewModel.kt
@@ -1,16 +1,34 @@
 package com.david.glez.firestoreadvancedxml.ui.addproduct
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class AddProductViewModel @Inject constructor(): ViewModel() {
+class AddProductViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState = MutableStateFlow(AddProductsUIState())
     val uiState: StateFlow<AddProductsUIState> = _uiState
+
+    fun onNameChanged(name: CharSequence?) {
+        _uiState.update { it.copy(name = name.toString()) }
+    }
+
+    fun onPriceChanged(price: CharSequence?) {
+        _uiState.update { it.copy(price = price.toString()) }
+    }
+
+    fun onDescriptionChanged(description: CharSequence?) {
+        _uiState.update { it.copy(description = description.toString()) }
+    }
+
+    fun onImageSelected(uri: Uri) {
+        //val result =
+    }
 }
 
 data class AddProductsUIState(
@@ -19,4 +37,6 @@ data class AddProductsUIState(
     val price: String = "",
     val imageURL: String = "",
     val isLoading: Boolean = false
-)
+) {
+    fun isValidProduct() = name.isNotBlank() && description.isNotBlank() && price.isNotBlank()
+}
